7099f62c9c96cdfcd2f008176fe73b3d1685a8eb
Nils Hesse <nphesse@gmail.com>
2014-09-15 15:08:23 +0200

    Add helper function to check an HTTP server for working compression

--- zsh/helpers.zsh
+++ zsh/helpers.zsh
@@ -7,3 +7,7 @@ function highlight()
     sed "s/$1/`tput smso`&`tput rmso`/gi" "${2:--}"
 }
 
+function check_compression {
+    curl -I -H 'Accept-Encoding: gzip,deflate' $1 | grep "Content-Encoding"
+}
+
