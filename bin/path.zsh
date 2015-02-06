28493626c9fd47977fb34a703ef3199836583409
Nils Hesse <nphesse@gmail.com>
2015-02-06 15:13:08 +0100

    Add arm-none-eabi/eclise paths to PATH

--- bin/path.zsh
+++ bin/path.zsh
@@ -1,1 +1,1 @@
-export PATH="$PATH:$HOME/.dotfiles/bin:$HOME/_3Dprintering/Slic3r/bin"
+export PATH="$PATH:$HOME/.dotfiles/bin:$HOME/_3Dprintering/Slic3r/bin:$HOME/gcc-arm-none-eabi/bin:$HOME/eclipse"
