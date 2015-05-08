a7cde6762d7a8b463e2baeeda3701b0db62ca94a
Nils Hesse <nphesse@gmail.com>
2015-05-08 11:27:18 +0200

    Add GOlang env variables; Add the MYVIMRC env variable

--- env.zsh
+++ env.zsh
@@ -1,2 +1,6 @@
 export XDG_CONFIG_HOME="~/.config"
 export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:~/.config/powerline"
+export GOPATH=/home/irrenhaus/.gocode
+export PATH="$PATH:$GOPATH/bin"
+
+export MYVIMRC=~/.vimrc
