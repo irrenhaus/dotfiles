8c7c1a1841bbdde7e7f7cf8713d162f6033a6960
Nils Hesse <nphesse@gmail.com>
2015-09-29 12:00:51 +0200

    Again: Try to fix the "~" directory creation

--- env.zsh
+++ env.zsh
@@ -1,6 +1,6 @@
-export XDG_CONFIG_HOME="~/.config"
-export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:~/.config/powerline"
-export GOPATH=/home/irrenhaus/.gocode
+#export XDG_CONFIG_HOME="~/.config"
+#export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:~/.config/powerline"
+export GOPATH=$HOME/.gocode
 export PATH="$PATH:$GOPATH/bin"
 
 export MYVIMRC=~/.vimrc
