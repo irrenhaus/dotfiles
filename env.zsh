97b1d9ca5dae40af7568100ba2590a932832ed2b
Nils Hesse <nphesse@gmail.com>
2016-04-11 17:17:38 +0200

    stuff

--- env.zsh
+++ env.zsh
@@ -1,5 +1,5 @@
-export XDG_CONFIG_HOME="~/.config"
-export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:~/.config/powerline"
+export XDG_CONFIG_HOME="$HOME/.config"
+export XDG_CONFIG_DIRS="${XDG_CONFIG_DIRS}:$HOME/.config/powerline"
 export GOPATH=/home/irrenhaus/.gocode
 export PATH="$PATH:$GOPATH/bin"
 
