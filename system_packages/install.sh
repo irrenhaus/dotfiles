df26f0219c453d58b4c00e2ade682043b6a2bf05
Nils Hesse <n.hesse@patagona.de>
2018-09-05 10:29:55 +0200

    MOAR TOOLS

--- system_packages/install.sh
+++ system_packages/install.sh
@@ -1,22 +1,12 @@
 #!/bin/bash -e
 
-function install_yaourt {
-    if [ -f /usr/bin/yaourt ]; then
-        echo "yaourt already installed; skipping"
+function install_yay {
+    if [ -f /usr/bin/yay ]; then
+        echo "yay already installed; skipping"
         return
     fi
 
-    $(cd /tmp &&\
-        curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/package-query.tar.gz &&\
-        tar -xvzf package-query.tar.gz &&\
-        cd package-query &&\
-        sudo makepkg -si)
-
-    $(cd /tmp &&\
-        curl -O https://aur.archlinux.org/cgit/aur.git/snapshot/yaourt.tar.gz &&\
-        tar -xvzf yaourt.tar.gz &&\
-        cd yaourt &&\
-        sudo makepkg -si)
+    (cd /tmp && git clone https://aur.archlinux.org/yay.git && makepkg -si)
 }
 
 if [ -f /etc/debian_version ]; then
@@ -31,12 +21,12 @@ if [ -f /etc/arch-release ]; then
     if [ -f /usr/bin/sudo ]; then
         SUDO="sudo bash -c"
     fi
-    $SUDO "pacman -S --noconfirm --needed base-devel sudo curl diff-so-fancy pv acpi lm_sensors python-pip"
-    install_yaourt
-    yaourt -S --needed fasd watchman python2-pywatchman nvm
-    sudo pip install git-sweep
+    $SUDO "pacman -S --noconfirm --needed base-devel sudo curl pv acpi lm_sensors python-pip"
+    install_yay
+    $SUDO "yay -S --noconfirm --needed diff-so-fancy fasd nvm git-sweep ncdu noti silver-searcher entr bat prettyping fd fzf"
     source /usr/share/nvm/init-nvm.sh
     nvm install 10
     nvm use 10
+    npm install -g tldr
 fi
 
