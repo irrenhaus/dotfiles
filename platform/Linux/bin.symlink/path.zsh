9cff99f4f8e63dc14724d600f9ae44d669587509
Nils Hesse <n.hesse@patagona.de>
2019-04-29 14:46:05 +0200

    Paths and rust stuff

--- platform/Linux/bin.symlink/path.zsh
+++
@@ -1,1 +0,0 @@
-export PATH="$PATH:$HOME/.dotfiles/bin:$HOME/_3Dprintering/Slic3r/bin:$HOME/gcc-arm-none-eabi/bin:$HOME/eclipse:$HOME/_apps/bin"
---
+++ rust/env.zsh
@@ -0,0 +1,2 @@
+# $(rustc --print sysroot)/lib/rustlib/src/rust/src
+export RUST_SRC_PATH="$HOME/.rustup/toolchains/stable-x86_64-unknown-linux-gnu/lib/rustlib/src/rust/src"
